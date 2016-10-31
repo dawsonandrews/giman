23102f13d9d3f4b773f3b92072c5bc4e580106d8
Pete Hawkins <pete@phawk.co.uk>
2016-10-31 16:49:42 +0000

    Trigger events on input rather than document

--- test/dummy/app/assets/javascripts/application.js
+++ test/dummy/app/assets/javascripts/application.js
@@ -13,4 +13,43 @@
 //= require jquery
 //= require jquery_ujs
 //= require giman/giman
-//= require_tree .
+//= require_self
+
+$(function() {
+
+  $("[data-giman-drop-upload]").each(function() {
+    var $dropbox = $(this),
+        $input = $('#'+$dropbox.attr('data-giman-drop-upload')),
+        $status = $dropbox.find('.upload-text'),
+        $form = $dropbox.closest("form"),
+        $btn = $form.find("input[type=submit]");
+
+    console.log("Setup ", $dropbox);
+
+    // Cache original text so we can change it
+    $status.attr("data-original-txt", $status.text());
+    $btn.attr("data-original-txt", $btn.val());
+
+    $input.on("directUpload:started", function(e) {
+      $btn.attr("disabled", true).val("Uploading...");
+    });
+
+    $input.on("directUpload:progress", function(e, args) {
+      $status.text("Uploading "+ args.percentLoaded +"%");
+    });
+
+    $input.on("directUpload:complete", function(e, args) {
+      $status.text("Upload complete!");
+      setTimeout(function() {
+        $status.text($status.attr("data-original-txt"));
+      }, 1000);
+
+      $btn.removeAttr("disabled").val($btn.attr("data-original-txt"));
+
+      if ($dropbox.attr('data-with-preview')) {
+        $("#"+$dropbox.attr('data-with-preview')).html('<img src="'+ args.fileData.url +'" style="max-width: 100%;">');
+      }
+    });
+  });
+
+});
